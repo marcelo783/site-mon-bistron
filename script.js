92a5e95da88da99981b616d4d2fc7aa3630f6dc7
marcelo783 <marcelo.oliveira7931@gmail.com>
2023-02-23 01:07:02 -0300

    modificações no layout do cardápio

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
+
+
+function scrollToElement(secao) {
+    const element = document.getElementById(secao);
+    const offsetTop = element.offsetTop;
+    window.scrollTo({
+      top: offsetTop,
+      behavior: 'smooth' ,
+
+
+    });
+    console.log("passou");
+}

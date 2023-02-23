1a87d04f32d0e56646f14328e950ead398575f69
marcelo783 <marcelo.oliveira7931@gmail.com>
2023-02-23 20:07:21 -0300

    alterações no link interno

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 document.querySelector(".hamburger").addEventListener("click", () =>
-    document.querySelector(".container").classList.toggle("show-menu")
+  document.querySelector(".container").classList.toggle("show-menu")
 );
 
 
 function scrollToElement(secao) {
-    const element = document.getElementById(secao);
-    const offsetTop = element.offsetTop;
-    window.scrollTo({
-      top: offsetTop,
-      behavior: 'smooth' ,
+  const element = document.getElementById(secao);
+  const offsetTop = element.offsetTop;
+  window.scrollTo({
+    top: offsetTop + 0,
+    behavior: 'smooth',
 
 
-    });
-    console.log("passou");
+  });
+  console.log("passou");
+
+
+
+
+
+
 }
